ffaeef1ed8aaa91d8a4c6145753a13ab19fbf9ee
dhruvp-8 <dhruvpatel5738@gmail.com>
2016-11-25 16:32:52 +0530

    Working on Ajax Search.

---
+++ AjaxSearch/search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ajax Search</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/JSON-AJAX/AjaxSearch/css/main.css" rel="stylesheet">
+    </head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+    <script src="/JSON-AJAX/AjaxSearch/js/app.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <body>
+        <div class="container-fluid">
+        <h2 class="text-center">Ajax Search</h2>
+        <hr>
+        </div>
+    </body>
+</html>
